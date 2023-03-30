86143a298075620e2caa91885b71da58f676d693
Tijin Abe Thomas <74398038+tijin-abe-thomas@users.noreply.github.com>
2023-03-30 14:30:59 +0530

    commit

--- crowdfunding/web-final/migrations/1_deploy_contracts.js
+++ crowdfunding/web-final/migrations/1_deploy_contracts.js
@@ -3,5 +3,8 @@ const Campaigns = artifacts.require("Campaigns.sol");
 
 module.exports = function(deployer) {
  deployer.deploy(User)  
+<<<<<<< Updated upstream
  deployer.deploy(Campaigns)
+=======
+>>>>>>> Stashed changes
 };
